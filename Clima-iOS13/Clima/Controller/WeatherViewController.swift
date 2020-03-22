f8ba6a93b03db1a4cc83a3a40e5252fb24a91181
Antonio Silva <antonio.silvajr23@gmail.com>
2020-03-21 23:05:48 -0300

    add clima prj

---
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.placeholder = "type something..."
+        //searchTextField.text
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "type something..."
+            return false
+        }
+    }
+    
+}
+
